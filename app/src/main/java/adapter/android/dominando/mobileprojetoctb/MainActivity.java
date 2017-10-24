d29b4678047bd411ae42030583acbb8bfd3e3e74
Caio Phillipe <cppdesouza@icloud.com>
2017-10-24 09:25:51 -0300

    Alteração do editText para password

--- app/src/main/java/adapter/android/dominando/mobileprojetoctb/MainActivity.java
+++ app/src/main/java/adapter/android/dominando/mobileprojetoctb/MainActivity.java
@@ -1,13 +1,27 @@
 package adapter.android.dominando.mobileprojetoctb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private View.OnClickListener tratadorDoBotao = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent it = new Intent(MainActivity.this, GerenteActivity.class);
+            startActivity(it);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnLogarID).setOnClickListener(tratadorDoBotao);
     }
 }
