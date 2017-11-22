5ebb6acd5c0c8d34d52ce7e7c71ac66371a1a6fa
RenatoRedes <renatoredes@live.com>
2017-11-22 04:40:20 -0200

    Logon CTB

--- app/src/main/java/adapter/android/dominando/mobileprojetoctb/Maps/ActivityPrincipalCTB.java
+++ app/src/main/java/adapter/android/dominando/mobileprojetoctb/Maps/ActivityPrincipalCTB.java
@@ -1,5 +1,6 @@
 package adapter.android.dominando.mobileprojetoctb.Maps;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,7 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import adapter.android.dominando.mobileprojetoctb.MainActivity;
 import adapter.android.dominando.mobileprojetoctb.R;
 
 public class ActivityPrincipalCTB extends AppCompatActivity
@@ -119,6 +123,13 @@ public class ActivityPrincipalCTB extends AppCompatActivity
             case R.id.nav_mapsGPS:
                 showFragment(new MapsFragment(),"MapsGPS");
                 break;
+
+            case R.id.nav_slideshow:
+//// chamar uma Activity a partir de um item de um navigation drawer
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                    break;
+
         }
 
         /*os itens de menus caso precise já temos aqui
