29802666bd6acbdaaa5081c3cc241be497fa605d
DESKTOP-U0KFV49\Jeferson <jefersonguerrajr@live.com>
2017-12-02 14:21:40 -0300

    sobre

---
+++ app/src/main/java/adapter/android/dominando/mobileprojetoctb/SobreActivity.java
@@ -0,0 +1,13 @@
+package adapter.android.dominando.mobileprojetoctb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
